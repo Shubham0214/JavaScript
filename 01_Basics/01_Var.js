3fec2e41c43cdbf1310683edcb90f4a3eefd5600
Shubham0214 <shubhedu0112@gmail.com>
2024-05-31 07:23:07 +0530

    update

---
+++ 01_Basics/01_Var.js
@@ -0,0 +1,28 @@
+const account_Id = 144875
+
+
+//declare a variable
+//use only 
+let accountEmail = "shubh@gmail.com"
+
+
+var accountpass = "4523647"
+
+accountCity = "Azamgarh"
+
+//account_Id = 2  // not allowed
+
+console.log(account_Id);
+
+accountEmail= "shubhkkk@gmail.com"
+accountpass= "2154532"
+accountCity="Vizag"
+
+
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+ */
+
+console.table([accountEmail , accountpass , accountCity])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3)  //we are using node js not browserr
+
+console.log(3+3) //code readability perfect
+
+console.log("Shubham")
+
+let name = "Shubh"
+
+let age = 22
+
+let isLogged = false
+
+let state;
+
+// number => 2 to power 53
+// biginit
+// string => ""
+// boolean => true / false
+// null => standalone vallue    --- object
+// undefined   
+// symbol => unique
+
+
+console.log(typeof undefined);
+
