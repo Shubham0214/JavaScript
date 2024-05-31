b48deb7e6b9c115164964120dd3cc0589e74362c
Shubham0214 <shubhedu0112@gmail.com>
2024-05-31 07:38:59 +0530

    update

--- 01_Basics/03_ConAndOper.js
+++ 01_Basics/03_ConAndOper.js
@@ -1,6 +1,41 @@
-let score = 33
+let score = "shubh"
 
 
 console.log(typeof score);
 console.log(typeof(score));
 
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//"33" => 33
+
+//"33abc" => NaN but type is number
+
+// true => 1 , false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsloggedIn);
+
+
+// 1=> true; 0=> false
+
+// "" => false
+
+// "shubh" => true
+
+let somenumber = 33 
+
+let numtostr = String(somenumber)
+
+console.log(numtostr);
+
+console.log(typeof numtostr);
+
+// num changee to string show this by using typeof function
